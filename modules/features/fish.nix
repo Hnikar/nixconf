08d3ac686b6734b2e26a6871bf523229fd47185e
Hnikar <140448550+Hnikar@users.noreply.github.com>
2026-04-19 21:20:37 +0200

    fix fish and add pure prompt

--- modules/features/fish.nix
+++ modules/features/fish.nix
@@ -1,11 +1,21 @@
 { self, inputs, ... }: {
   flake.nixosModules.fish = { pkgs, username, ...}: {
+    
+    # 1. Enable fish system-wide (This fixes the error)
+    programs.fish.enable = true; 
+
+    # 2. Configure fish for your specific user via Home Manager
     home-manager.users.${username} = {
-    programs.fish = {
-      enable = true;
-      package = pkgs.fish;
+      programs.fish = {
+        enable = true;
+        package = pkgs.fish;
+        plugins = [
+          {
+            name = "pure";
+            src = pkgs.fishPlugins.pure.src;
+          }];
+      };
     };
-  };
-  users.${username}.shell = pkgs.fish;
+    
   };
 }
