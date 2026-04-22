91087439cc833f2f11ed737d6fbeee6fa8eb13a6
Hnikar <140448550+Hnikar@users.noreply.github.com>
2026-04-22 06:42:56 +0200

    expand niri keyboard bindings

--- modules/features/DE/niri.nix
+++ modules/features/DE/niri.nix
@@ -9,29 +9,119 @@
   perSystem = { pkgs, lib, self', ... }: {
     packages.myNiri = inputs.wrapper-modules.wrappers.niri.wrap {
       inherit pkgs; # THIS PART IS VERY IMPORTAINT, I FORGOT IT IN THE VIDEO!!!
-      v2-settings = true;
+      #v2-settings = true;
       settings = {
         spawn-at-startup = [
           (lib.getExe self'.packages.myNoctalia)
         ];
-	#v2-settings = true;
         xwayland-satellite.path = lib.getExe pkgs.xwayland-satellite;
 
- 	input.keyboard = {
-            xkb = {
-              layout = "us,ru";
-              options = "grp:alt_shift_toggle,caps:escape";
-            };
-            repeat-rate = 40;
-            repeat-delay = 250;
+        input.keyboard = {
+          xkb = {
+            layout = "us,ru";
+            options = "grp:alt_shift_toggle,caps:escape";
           };
+          repeat-rate = 40;
+          repeat-delay = 250;
+        };
         layout.gaps = 5;
 
         binds = {
-          "Mod+T".spawn-sh = lib.getExe pkgs.ghostty;
-	  
-          "Mod+Q".close-window = {};
+          # Terminal
+          "Mod+Return".spawn-sh = lib.getExe pkgs.ghostty;
+
+          # Apps
+          "Mod+E".spawn-sh = lib.getExe pkgs.nautilus;
+          "Mod+B".spawn-sh = lib.getExe pkgs.brave;
+
+          # Core
           "Mod+S".spawn-sh = "${lib.getExe self'.packages.myNoctalia} ipc call launcher toggle";
+          "Mod+Space".spawn-sh = "${lib.getExe self'.packages.myNoctalia} ipc call controlCenter toggle";
+          "Mod+Comma".spawn-sh = "${lib.getExe self'.packages.myNoctalia} ipc call settings toggle";
+          "Mod+Q".close-window = {};
+
+          # Window & Column Navigation
+          ## Using Arrows
+          "Mod+Left".focus-column-left = {};
+          "Mod+Right".focus-column-right = {};
+          "Mod+Up".focus-window-up = {};
+          "Mod+Down".focus-window-down = {};
+          
+          ## Using Vi-keys (HJKL)
+          "Mod+H".focus-column-left = {};
+          "Mod+L".focus-column-right = {};
+          "Mod+K".focus-window-up = {};
+          "Mod+J".focus-window-down = {};
+          
+          "Mod+Home".focus-column-first = {};
+          "Mod+End".focus-column-last = {};
+
+          # Monitor Navigation
+          "Mod+Shift+Left".focus-monitor-left = {};
+          "Mod+Shift+Right".focus-monitor-right = {};
+          "Mod+Shift+Up".focus-monitor-up = {};
+          "Mod+Shift+Down".focus-monitor-down = {};
+          
+          "Mod+Shift+Ctrl+Left".move-column-to-monitor-left = {};
+          "Mod+Shift+Ctrl+Right".move-column-to-monitor-right = {};
+
+          #  Layout & Window Modes
+          "Mod+F".maximize-column = {};
+          "Mod+Shift+F".fullscreen-window = {};
+          "Mod+C".center-column = {};
+          "Mod+Minus".set-column-width = "-10%";
+          "Mod+Equal".set-column-width = "+10%";
+          "Mod+Shift+Minus".set-window-height = "-10%";
+          "Mod+Shift+Equal".set-window-height = "+10%";
+          
+          "Mod+T".toggle-window-floating = {};
+          "Mod+W".toggle-column-tabbed-display = {};
+          "Mod+O".toggle-overview = {};
+
+          # Workspace Management
+          "Mod+1".focus-workspace = 1;
+          "Mod+2".focus-workspace = 2;
+          "Mod+3".focus-workspace = 3;
+          "Mod+4".focus-workspace = 4;
+          "Mod+5".focus-workspace = 5;
+          "Mod+6".focus-workspace = 6;
+          "Mod+7".focus-workspace = 7;
+          "Mod+8".focus-workspace = 8;
+          "Mod+9".focus-workspace = 9;
+          "Mod+Tab".focus-workspace-previous = {};  
+
+          "Mod+Shift+1".move-column-to-workspace = 1;
+          "Mod+Shift+2".move-column-to-workspace = 2;
+          "Mod+Shift+3".move-column-to-workspace = 3;
+          "Mod+Shift+4".move-column-to-workspace = 4;
+          "Mod+Shift+5".move-column-to-workspace = 5;
+          "Mod+Shift+6".move-column-to-workspace = 6;
+          "Mod+Shift+7".move-column-to-workspace = 7;
+          "Mod+Shift+8".move-column-to-workspace = 8;
+          "Mod+Shift+9".move-column-to-workspace = 9;
+
+          # Scroll Navigation
+          "Mod+WheelScrollDown".focus-column-left = {};
+          "Mod+WheelScrollUp".focus-column-right = {};
+          "Mod+Ctrl+WheelScrollDown".focus-workspace-down = {};
+          "Mod+Ctrl+WheelScrollUp".focus-workspace-up = {};
+
+          # Screenshots
+          "Print".screenshot = {};
+          "Ctrl+Print".screenshot-screen = {};
+          "Alt+Print".screenshot-window = {};
+
+          # Misc
+          "Mod+Shift+Escape".show-hotkey-overlay = {};
+          #"Mod+Shift+P".power-off-monitors = {};
+          #"Ctrl+Alt+Delete".quit = {};
+
+          # Media
+          "XF86AudioRaiseVolume".spawn-sh = "${lib.getExe self'.packages.myNoctalia} ipc call volume increase";
+          "XF86AudioLowerVolume".spawn-sh = "${lib.getExe self'.packages.myNoctalia} ipc call volume decrease";
+          "XF86AudioMute".spawn-sh = "${lib.getExe self'.packages.myNoctalia} ipc call volume muteOutput";
+          "XF86MonBrightnessUp".spawn-sh = "${lib.getExe self'.packages.myNoctalia} ipc call brightness increase";
+          "XF86MonBrightnessDown".spawn-sh = "${lib.getExe self'.packages.myNoctalia} ipc call brightness decrease";
         };
       };
     };
