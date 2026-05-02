b3cbb53d399a58b84c418574d81da864b81bd518
Hnikar <140448550+Hnikar@users.noreply.github.com>
2026-05-02 04:36:13 +0000

    fix: uncomment necessary configurations in steam module

--- modules/features/desktopApps/steam.nix
+++ modules/features/desktopApps/steam.nix
@@ -1,12 +1,12 @@
 { self, inputs, ... }: {
   flake.nixosModules.steam = { pkgs, ...}: {
-    #nixpkgs.overlays = [ inputs.millennium.overlays.default ];
+    nixpkgs.overlays = [ inputs.millennium.overlays.default ];
     programs.steam = {
       enable = true;
-      #package = pkgs.millennium-steam;
+      package = pkgs.millennium-steam;
       extraCompatPackages = [ pkgs.proton-ge-bin ];
     };
     hardware.steam-hardware.enable = true;
-    #services.joycond.enable = true;
+    services.joycond.enable = true;
   };
 }
