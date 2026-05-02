0475ea83dfe43fa0b9c48e78508fb687c8457703
Hnikar <140448550+Hnikar@users.noreply.github.com>
2026-05-02 04:19:23 +0000

    fix: remove commented-out default applications in defaultApps module

--- modules/features/misc/defaultApps.nix
+++ modules/features/misc/defaultApps.nix
@@ -5,11 +5,6 @@
       xdg.mimeApps = {
       enable = true;
       defaultApplications = {
-        # "text/html" = "librewolf.desktop";
-        # "x-scheme-handler/http" = "librewolf.desktop";
-        # "x-scheme-handler/https" = "librewolf.desktop";
-        # "x-scheme-handler/about" = "librewolf.desktop";
-        # "x-scheme-handler/unknown" = "librewolf.desktop";
         "inode/directory" = "org.gnome.Nautilus.desktop";
       };
     };
