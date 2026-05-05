bac3b2646f75140a61b3c051162267d1f4c6d4f9
Hnikar <140448550+Hnikar@users.noreply.github.com>
2026-05-05 05:08:10 +0200

    remove home-manager user configuration from fonts module

--- modules/features/misc/fonts.nix
+++ modules/features/misc/fonts.nix
@@ -2,12 +2,10 @@
 { self, inputs, ... }: 
 {
   flake.nixosModules.fonts = { pkgs, username, ... }: {
-    home-manager.users.${username} = {
       fonts.packages = with pkgs; [
         noto-fonts
         noto-fonts-cjk-sans
         noto-fonts-color-emoji
       ];
-    };
   };
 }
