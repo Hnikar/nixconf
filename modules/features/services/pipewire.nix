5132ea27ffe6abd02a3a4f66261c99ab6ff9b4e2
Hnikar <140448550+Hnikar@users.noreply.github.com>
2026-05-10 15:14:38 +0200

    feat: enhance PipeWire configuration with additional audio routing options

--- modules/features/services/pipewire.nix
+++ modules/features/services/pipewire.nix
@@ -1,19 +1,44 @@
 { self, inputs, ... }:
 {
   flake.nixosModules.pipewire = { pkgs, ... }:
-    {
-      environment.systemPackages = [ pkgs.pavucontrol ];
+  {
+    environment.systemPackages = with pkgs; [ 
+      pavucontrol 
+      qpwgraph # Optional, but highly recommended for visual audio routing
+    ];
       services.pipewire.extraConfig.pipewire."91-null-sinks" = {
-        "context.objects" = [
-          {
-            factory = "spa-node-factory";
-            args = {
-              "factory.name" = "support.node.driver";
-              "node.name" = "Dummy-Driver";
-              "priority.driver" = 8000;
-            };
-          }
-        ];
-      };
-    };
+    "context.objects" = [
+      {
+        # A default dummy driver. This handles nodes marked with the "node.always-driver"
+        # properyty when no other driver is currently active. JACK clients need this.
+        factory = "spa-node-factory";
+        args = {
+          "factory.name" = "support.node.driver";
+          "node.name" = "Dummy-Driver";
+          "priority.driver" = 8000;
+        };
+      }
+      {
+        factory = "adapter";
+        args = {
+          "factory.name" = "support.null-audio-sink";
+          "node.name" = "Microphone-Proxy";
+          "node.description" = "Microphone";
+          "media.class" = "Audio/Source/Virtual";
+          "audio.position" = "MONO";
+        };
+      }
+      {
+        factory = "adapter";
+        args = {
+          "factory.name" = "support.null-audio-sink";
+          "node.name" = "Main-Output-Proxy";
+          "node.description" = "Main Output";
+          "media.class" = "Audio/Sink";
+          "audio.position" = "FL,FR";
+        };
+      }
+    ];
+  };
+  };
 }
