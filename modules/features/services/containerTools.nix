112d5c91494f574aa9b2f35dd7a73eeeee0044ad
Hnikar <140448550+Hnikar@users.noreply.github.com>
2026-05-11 00:06:54 +0200

    feat: add podman-desktop to system packages and enable docker support

--- modules/features/services/containerTools.nix
+++ modules/features/services/containerTools.nix
@@ -4,12 +4,14 @@
    environment.systemPackages = with pkgs; [
       distrobox
       distroshelf
+      podman-desktop
    ];
     virtualisation = {
-    containers.enable = true;
+      containers.enable = true;
+      docker.enable = true;
       podman = {
         enable = true;
-        dockerCompat = true;
+        #dockerCompat = true;
         defaultNetwork.settings.dns_enabled = true; # Required for containers under podman-compose to be able to talk to each other.
       };
     };
