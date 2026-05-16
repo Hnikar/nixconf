bf3f2905512543723ffe167122c5823768dba300
Hnikar <140448550+Hnikar@users.noreply.github.com>
2026-05-16 23:52:30 +0200

    expand nvf settings

--- modules/features/applications/vim.nix
+++ modules/features/applications/vim.nix
@@ -6,13 +6,21 @@
       imports = [ inputs.nvf.nixosModules.default ];
       programs.nvf = {
         enable = true;
-        # Your settings need to go into the settings attribute set
-        # most settings are documented in the appendix
+
         settings = {
-          vim.viAlias = false;
-          vim.vimAlias = true;
-          vim.lsp = {
-            enable = true;
+          vim = {
+            viAlias = false;
+            vimAlias = true;
+            lsp.enable = true;
+            treesitter.enable = true;
+            theme = {
+              enable = true;
+              name = "gruvbox";
+              style = "dark";
+            };
+            statusline.lualine.enable = true;
+            telescope.enable = true;
+            autocomplete.nvim-cmp.enable = true;
           };
         };
       };
