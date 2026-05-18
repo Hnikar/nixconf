b450f999ad9326de54df5c67539005b47170d289
Hnikar <140448550+Hnikar@users.noreply.github.com>
2026-05-18 15:34:53 +0200

    add flatpak module

---
+++ modules/features/services/flatpak.nix
@@ -0,0 +1,23 @@
+{
+  self,
+  inputs,
+  ...
+}:
+{
+  flake.nixosModules.flatpak =
+    {
+      pkgs,
+      username,
+      ...
+    }:
+    {
+      services.flatpak.enable = true;
+      systemd.services.flatpak-repo = {
+        wantedBy = [ "multi-user.target" ];
+        path = [ pkgs.flatpak ];
+        script = ''
+          flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+        '';
+      };
+    };
+}
