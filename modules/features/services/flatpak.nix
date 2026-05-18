a86b015253bb3afd9c497f63e8b9dc86ebd94f45
Hnikar <140448550+Hnikar@users.noreply.github.com>
2026-05-18 15:43:16 +0200

    add nix-flatpak module and integrate into flake configuration

--- modules/features/services/flatpak.nix
+++ modules/features/services/flatpak.nix
@@ -11,13 +11,10 @@
       ...
     }:
     {
+      imports = [ nix-flatpak.nixosModules.nix-flatpak ];
       services.flatpak.enable = true;
-      systemd.services.flatpak-repo = {
-        wantedBy = [ "multi-user.target" ];
-        path = [ pkgs.flatpak ];
-        script = ''
-          flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-        '';
-      };
+      services.flatpak.packages = [
+        "net.lutris.Lutris"
+      ];
     };
 }
