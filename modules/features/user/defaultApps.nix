57e41b3997693226176950544c72fc5e498cb657
Hnikar <140448550+Hnikar@users.noreply.github.com>
2026-07-01 13:15:03 +0200

    refactor: improve formatting and structure of default applications configuration

--- modules/features/user/defaultApps.nix
+++ modules/features/user/defaultApps.nix
@@ -1,14 +1,26 @@
-{ self, inputs, ... }: 
+{ self, inputs, ... }:
 {
-  flake.nixosModules.defaultApps = { pkgs, username, ... }: {
-    home-manager.users.${username} = {
-      xdg.mimeApps = {
-      enable = true;
-      defaultApplications = {
-        "inode/directory" = "org.gnome.Nautilus.desktop";
-        "application/pdf" = "org.gnome.Papers.desktop";
+  flake.nixosModules.defaultApps =
+    { pkgs, username, ... }:
+    {
+      home-manager.users.${username} = {
+        xdg.mimeApps = {
+          enable = true;
+          defaultApplications = {
+            "inode/directory" = "org.gnome.Nautilus.desktop";
+            "application/pdf" = "org.gnome.Papers.desktop";
+            "image/avif" = "org.gnome.Loupe.desktop";
+            "image/bmp" = "org.gnome.Loupe.desktop";
+            "image/gif" = "org.gnome.Loupe.desktop";
+            "image/heic" = "org.gnome.Loupe.desktop";
+            "image/jpeg" = "org.gnome.Loupe.desktop";
+            "image/jxl" = "org.gnome.Loupe.desktop";
+            "image/png" = "org.gnome.Loupe.desktop";
+            "image/svg+xml" = "org.gnome.Loupe.desktop";
+            "image/tiff" = "org.gnome.Loupe.desktop";
+            "image/webp" = "org.gnome.Loupe.desktop";
+          };
+        };
       };
     };
-    };
-  };
 }
