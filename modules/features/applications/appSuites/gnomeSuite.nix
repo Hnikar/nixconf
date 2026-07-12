838a68da4d77bee2f5cbbd1514cd7fe053fc93b5
Hnikar <140448550+Hnikar@users.noreply.github.com>
2026-07-12 21:08:02 +0200

    refactor: restructure gnomeSuite configuration and add GStreamer plugins

--- modules/features/applications/appSuites/gnomeSuite.nix
+++ modules/features/applications/appSuites/gnomeSuite.nix
@@ -1,20 +1,48 @@
-{ self, inputs, ... }: 
+{ self, inputs, ... }:
 {
-  flake.nixosModules.gnomeSuite = { pkgs, ... }: {
-    environment.systemPackages = with pkgs; [
-      nautilus
-      gnome-text-editor
-      showtime
-      gnome-calculator
-      loupe
-      papers
-      showtime
-      snapshot
-      decibels
-      gnome-boxes
-      pika-backup
-      baobab
-      gnome-disk-utility
-    ];
-  };
+  flake.nixosModules.gnomeSuite =
+    { pkgs, ... }:
+    {
+      nixpkgs.overlays = [
+        (final: prev: {
+          nautilus = prev.nautilus.overrideAttrs (old: {
+            buildInputs =
+              old.buildInputs
+              ++ (with prev.gst_all_1; [
+                gst-plugins-good
+                gst-plugins-bad
+              ]);
+          });
+        })
+      ];
+
+      services.gnome.sushi.enable = true;
+
+      environment.systemPackages = with pkgs; [
+        nautilus
+        gnome-text-editor
+
+        ffmpegthumbnailer
+
+        showtime
+        gnome-calculator
+        loupe
+        papers
+        snapshot
+        decibels
+        gnome-boxes
+        pika-backup
+        baobab
+        gnome-disk-utility
+
+        ###
+
+        gst_all_1.gstreamer
+        gst_all_1.gst-plugins-base
+        gst_all_1.gst-plugins-good
+        gst_all_1.gst-plugins-bad
+        gst_all_1.gst-plugins-ugly
+        gst_all_1.gst-libav
+      ];
+    };
 }
