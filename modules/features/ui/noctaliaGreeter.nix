09317a73cb2ce0660fd4e501b3ccf44903018825
Hnikar <140448550+Hnikar@users.noreply.github.com>
2026-08-23 19:17:15 +0200

    fix noctalia greeter

--- modules/features/ui/noctaliaGreeter.nix
+++ modules/features/ui/noctaliaGreeter.nix
@@ -1,31 +1,27 @@
+{ self, inputs, ... }:
 {
-  self,
-  inputs,
-  lib,
-  pkgs,
-  ...
-}:
-{
-  flake.nixosModules.noctaliaGreeter = {
-    imports = [
-      inputs.noctalia-greeter.nixosModules.default
-    ];
+  flake.nixosModules.noctaliaGreeter =
+    { pkgs, lib, ... }:
+    {
+      imports = [
+        inputs.noctalia-greeter.nixosModules.default
+      ];
 
-    programs.noctalia-greeter = {
-      enable = lib.mkDefault true;
-      settings = lib.mkDefault {
-        session.default = "niri";
+      programs.noctalia-greeter = {
+        enable = lib.mkDefault true;
+        settings = lib.mkDefault {
+          session.default = "niri";
 
-        cursor = {
-          theme = "phinger-cursors-dark";
-          size = 24;
-          path = "${pkgs.phinger-cursors}/share/icons";
-        };
+          cursor = {
+            theme = "phinger-cursors-dark";
+            size = 24;
+            path = "${pkgs.phinger-cursors}/share/icons";
+          };
 
-        keyboard = {
-          layout = "us";
+          keyboard = {
+            layout = "us";
+          };
         };
       };
     };
-  };
 }
