fc14b22ee9ed4bc5917e8366d3f65aea28bbaa53
Hnikar <140448550+Hnikar@users.noreply.github.com>
2026-09-22 01:16:16 +0200

    fix: new noctalia greeter syntax

--- modules/features/ui/noctaliaGreeter.nix
+++ modules/features/ui/noctaliaGreeter.nix
@@ -7,7 +7,7 @@
         inputs.noctalia-greeter.nixosModules.default
       ];
 
-      programs.noctalia-greeter = {
+      services.displayManager."noctalia-greeter" = {
         enable = lib.mkDefault true;
         settings = lib.mkDefault {
           session.default = "niri";
